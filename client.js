e57796d8e9790bd14bd10733a0c614b0379f2c40
Joseph Gentle <me@josephg.com>
2016-06-26 19:13:25 +1000

    Added trait visibility

--- client.js
+++ client.js
@@ -101,8 +101,16 @@ const roundOverview = (params, state, send) => {
   console.log(params);
   console.log(state.people);
 
+  var remaining = 0;
+  for (var person in state.people) {
+    if (!person.removedInRound) remaining++;
+  }
+
   const round = state.round;
+  const targetRemaining = ({1: 20, 2: 15, 3: 10})[round];
+
   const next = () => {
+    //if (remaining !== targetRemaining) return;
     if (round < 3) send('round', {round: round + 1});
     else send('navigate', {location: '/outro'});
   };
@@ -110,14 +118,17 @@ const roundOverview = (params, state, send) => {
   return choo.view`
     <div>
       <h1>Round ${round}</h1>
+      <h2>${remaining} people remaining. Banish ${remaining - targetRemaining} to progress</h2>
       <ul>
         ${objAsSortedList(state.people, 'sortOrder')
         .filter(person => !person.removedInRound || person.removedInRound === round)
         .map(person => choo.view`
-          <li><a href=detail/${person.id}>${person.id}</a></li>
+          <li style=${person.removedInRound ? 'text-decoration: line-through' : ''}>
+            <a href=detail/${person.id}>${person.id}</a>
+          </li>
         `)}
       </ul>
-      <button onclick=${next}>${round < 3 ? 'Next round' : 'Submit your choices'}</button>
+      <button onclick=${next} disabled=${remaining !== targetRemaining,false}>${round < 3 ? 'Next round' : 'Submit your choices'}</button>
     </div>
   `;
 };
@@ -141,6 +152,11 @@ const roundDetails = (params, state, send) => {
   return choo.view`<div>
     A person ${person.id}
     <div style="width: 200px; height: 200px; background-color: grey; border: 1px black;">Cool image</div>
+    <div>
+      ${person.details.stages
+        .slice(0, state.round)
+        .map(text => choo.view`<div>${text}</div>`)}
+    </div>
     <label>${checkbox} Remove</label>
     <br>
     <a href='../overview'>Back</a>
